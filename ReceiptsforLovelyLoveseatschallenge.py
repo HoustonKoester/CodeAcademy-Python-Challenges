8b77a61f4f66599e22981630b65a960242c39087
TexasKing <67532088+TexasKing@users.noreply.github.com>
2021-03-28 23:48:08 -0700

    Basic Math Coding Challenge

--- ReceiptsforLovelyLoveseatschallenge.py
+++ ReceiptsforLovelyLoveseatschallenge.py
@@ -1,3 +1,5 @@
+#basic math coding challenge in CodeAcademy by Houston Koester
+
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white."
 lovely_loveseat_price = 254.00
 stylish_sette_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
